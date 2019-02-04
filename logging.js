fbbd63d15a00016088af85a135f7b1c543959eab
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-02-04 13:31:42 +0000

    logging.js

--- logging.js
+++ logging.js
@@ -3,31 +3,31 @@ var esprima = require('esprima');
 var estraverse = require('estraverse');
 
 function addLogging(code) {
-  var ast = esprima.parse(code);
-  estraverse.traverse(ast, {
-    enter: function(node, parent) {
-      if (node.type === 'FunctionDeclaration'
-          || node.type === 'FunctionExpression') {
-        addBeforeCode(node);
-      }
-    }
-  });
-  return escodegen.generate(ast);
+    var ast = esprima.parse(code);
+    estraverse.traverse(ast, {
+        enter: function(node, parent) {
+            if (node.type === 'FunctionDeclaration' ||
+                node.type === 'FunctionExpression') {
+                addBeforeCode(node);
+            }
+        }
+    });
+    return escodegen.generate(ast);
 }
 
 function addBeforeCode(node) {
-  var name = node.id ? node.id.name : '<anonymous function>';
-  var beforeCode = "console.log('Entering " + name + "()');";
-  var beforeNodes = esprima.parse(beforeCode).body;
-  node.body.body = beforeNodes.concat(node.body.body);
+    var name = node.id ? node.id.name : '<anonymous function>';
+    var beforeCode = "console.log('Entering " + name + "()');";
+    var beforeNodes = esprima.parse(beforeCode).body;
+    node.body.body = beforeNodes.concat(node.body.body);
 }
 
-console.log(addLogging("  \
-function foo(a, b) {      \
-  var x = 'blah';         \
-  var y = (function () {  \
-    return 3;             \
-  })();                   \
-}                         \
-foo(1, 'wut', 3);         \
-"));
+console.log(addLogging(`
+function foo(a, b) {   
+  var x = 'blah';   
+  var y = (function () {
+    return 3;
+  })();
+}     
+foo(1, 'wut', 3);
+`));
