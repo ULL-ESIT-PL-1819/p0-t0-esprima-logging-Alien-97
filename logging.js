c0b360aa58c9426133d95d0e26056f8f6b1729d4
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2019-02-06 08:20:24 +0000

    logging.js

--- logging.js
+++ logging.js
@@ -1,9 +1,9 @@
-var escodegen = require('escodegen');
-var esprima = require('esprima');
-var estraverse = require('estraverse');
+let escodegen = require('escodegen');
+let esprima = require('esprima');
+let estraverse = require('estraverse');
 
 function addLogging(code) {
-    var ast = esprima.parse(code);
+    let ast = esprima.parse(code);
     estraverse.traverse(ast, {
         enter: function(node, parent) {
             if (node.type === 'FunctionDeclaration' ||
@@ -16,20 +16,19 @@ function addLogging(code) {
 }
 
 function addBeforeCode(node) {
-    var name = node.id ? node.id.name : '<anonymous function>';
-    var beforeCode = "console.log('Entering " + name + "()');";
-    var beforeNodes = esprima.parse(beforeCode).body;
+    let name = node.id ? node.id.name : '<anonymous function>';
+    let beforeCode = `console.log('Entering ${name}()');`;
+    let beforeNodes = esprima.parse(beforeCode).body;
     node.body.body = beforeNodes.concat(node.body.body);
 }
 
-
 const input = `
-function foo(a, b) {   
-  var x = 'blah';   
-  var y = (function () {
+function foo(a, b) {
+  let x = 'blah';
+  let y = (function () {
     return 3;
   })();
-}     
+}
 foo(1, 'wut', 3);
 `;
 
